4091b991185d4513288b32fca92c211df2332600
joseeduardoleite <jose.eduardo1201@outlook.com>
2022-07-06 22:14:37 -0300

    children routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'restaurants', component: RestaurantsComponent },
-  { path: 'restaurants/:id', component: RestaurantDetailComponent },
+  { path: 'restaurants/:id', component: RestaurantDetailComponent,
+    children: [
+      { path: '', redirectTo: 'menu', pathMatch: 'full' },
+      { path: 'menu', component: MenuComponent },
+      { path: 'reviews', component: ReviewsComponent }
+    ]},
   { path: 'about', component: AboutComponent }
 ];
 
---
+++ src/app/restaurant-detail/reviews/reviews.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mt-reviews',
+  templateUrl: './reviews.component.html'
+})
+export class ReviewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {  }
+}
